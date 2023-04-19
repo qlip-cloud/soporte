bcd9439b1982803ead0fd558617fd98a1915012e
CaMentum <camilo.otalora@mentum.group>
2023-04-19 16:40:20 +0000

    Se agregaron 2 doc-types y se agregaron como respuestas al doctype de soporte

---
+++ support_portal/support_portal/doctype/support_product/support_product.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Mentum and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Support product', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ support_portal/support_portal/doctype/support_product/test_support_product.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, Mentum and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestSupportproduct(unittest.TestCase):
+	pass
