59f8f1b195342725a79aed40ffa2b8b457c94fbf
HendersonMentum <henderson.villegas@mentum.group>
2024-09-30 20:36:48 +0000

    add changes

--- support_portal/use_case/tickets/save.py
+++ support_portal/use_case/tickets/save.py
@@ -6,6 +6,29 @@ from  frappe.desk.form.utils import add_comment
 def handler(subject, producto ,priority, tipo, description, comment):
 
     doc = frappe.new_doc('Issue')
+
+    contact_doc_name = frappe.db.get_value('Contact', filters={'user': frappe.session.user_email}, fieldname=['name'])
+
+    if contact_doc_name:
+      client_doc_name = frappe.db.get_value('Dynamic Link', filters={'parenttype': 'Contact', 'link_doctype':'Customer', 'parent':contact_doc_name}, fieldname=['link_name'],)
+
+      if client_doc_name:
+            band_support_product_list = frappe.db.get_list('Dynamic Link', filters={'parenttype': 'Cliente', 'link_doctype':'Band_Support_Product', 'parent':client_doc_name}, fields=['*'],)
+
+            exist = False
+
+            if band_support_product_list:
+                for b in band_support_product_list:
+                    if b.support_product == producto:
+                        exist = True
+                        doc.band = b.band
+                        break
+                    
+            if not exist:
+               client_band = frappe.db.get_value('Cliente', filters={'name':client_doc_name}, fieldname=['band'],)
+               doc.band = client_band
+    
+    
     doc.subject = subject
     doc.priority = priority
     doc.tipo = tipo
