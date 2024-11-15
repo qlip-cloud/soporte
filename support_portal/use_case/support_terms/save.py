53aa4e9d952af845c4623c1a2e1808dc6750c795
HendersonMentum <henderson.villegas@mentum.group>
2024-11-15 20:07:28 +0000

    add changes

--- support_portal/use_case/support_terms/save.py
+++ support_portal/use_case/support_terms/save.py
@@ -12,15 +12,17 @@ def update(support_terms, method):
 
     tah = 0
     tih = 0
+    cas = 0
 
     for t in sup_ter:
-        if t.status == _("Closed"):
+        if t.status == "Closed":
             tah += t.horas_aplicadas
             tih += t.horas_facturadas 
+            cas += 1
 
     if support_terms.tipo_de_asignacion == "Número de casos":
-        support_terms.cantidad_aplicada = len(sup_ter)
-        support_terms.cantidad_facturada = len(sup_ter)
+        support_terms.cantidad_aplicada = cas
+        support_terms.cantidad_facturada = cas
     if support_terms.tipo_de_asignacion == "Número de Horas":
         support_terms.cantidad_aplicada = tah
         support_terms.cantidad_facturada = tih
