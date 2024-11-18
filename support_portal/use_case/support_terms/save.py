ddad3695e2e86ef9dfe983dd9f3550cb41f96336
HendersonMentum <henderson.villegas@mentum.group>
2024-11-18 17:32:19 +0000

    add changes to support terms and issue invoice

--- support_portal/use_case/support_terms/save.py
+++ support_portal/use_case/support_terms/save.py
@@ -19,7 +19,7 @@ def update(support_terms, method):
         tah += t.horas_aplicadas
         tih += t.horas_facturadas 
         casa += 1
-        if support_terms.tipo_de_asignacion == "Número de casos" and sup_ter.facturada == True:
+        if support_terms.tipo_de_asignacion == "Número de casos" and sup_ter.facturable == True:
                 casf += 1
 
     if support_terms.tipo_de_asignacion == "Número de casos":
