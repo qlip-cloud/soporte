362b2bc81aa3152117425eb303d40f331098f51d
HendersonMentum <henderson.villegas@mentum.group>
2025-01-03 18:13:08 +0000

    support changes to issues

--- support_portal/www/tickets/mine.py
+++ support_portal/www/tickets/mine.py
@@ -3,12 +3,14 @@ from support_portal.services.get_customer_id import handler as get_customer_id
 
 def get_context(context):
     
+    print(context)
+
     #frappe.clear_cache()
         
     #frappe.website.render.clear_cache()
     
     context.no_cache = 1
-    context.issues = frappe.db.get_list("Issue", filters = {"customer": get_customer_id()}, fields = ["*"])
+    context.issues = frappe.db.get_list("Issue", filters = {"customer": get_customer_id(), "raised_by":frappe.session.user}, fields = ["*"])
 
     
 
