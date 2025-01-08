170fb43566b679c71e597f4439cc591278dbc05d
HendersonMentum <henderson.villegas@mentum.group>
2025-01-08 22:58:24 +0000

    add changes to issue list

--- support_portal/www/tickets/all.py
+++ support_portal/www/tickets/all.py
@@ -13,6 +13,8 @@ def get_context(context):
 
     query_params = frappe.request.args
 
+    print(query_params)
+    
     context.customers = get_customer_id()
 
     if query_params.get("customer"):
@@ -23,7 +25,6 @@ def get_context(context):
     context.issues = frappe.db.get_list("Issue", filters = {"customer": context.customer}, order_by='creation desc', fields = ["*"])
 
     for key, issue in enumerate(context.issues):
-        print(issue._assign)
         context.issues[key].assign = json.loads(issue._assign)[0] if issue._assign else ''
         context.issues[key].creation = format_datetime(issue.creation,format='short', locale='es_CO')
         context.issues[key].modified = format_datetime(issue.modified,format='short', locale='es_CO')
