3f0ad122d153c1c39f9e00835ab146ba078a729b
HendersonMentum <henderson.villegas@mentum.group>
2025-03-25 14:24:19 +0000

    add changes to issues support terms

--- support_portal/public/js/issue.js
+++ support_portal/public/js/issue.js
@@ -2,8 +2,8 @@ frappe.ui.form.on("Issue", {
     customer: function(frm, cdt, cdn){
         frm.set_query("support_terms", function() {
             return {
-                "filters":"support_portal.support_portal.services.support_terms.handler",
-                "filters": {"cliente": frm.doc.customer}
+                query:"support_portal.support_portal.services.support_terms.handler",
+                filters: {"cliente": frm.doc.customer}
             };
         });
         frm.refresh_fields()
