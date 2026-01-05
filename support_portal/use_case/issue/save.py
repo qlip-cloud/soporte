982da0e52b77aa2f279a6c8564e313447e35c501
Ashley <ashleydefort@outlook.es>
2026-01-05 22:10:52 +0000

    chore: remove support terms related task modifications

--- support_portal/use_case/issue/save.py
+++ support_portal/use_case/issue/save.py
@@ -26,8 +26,8 @@ def update(issue, method):
 
         support_term = frappe.get_doc('Support Terms', issue.support_terms)
 
-        if support_term.restringir == "Si" or support_term.fecha_de_finalizacion <= frappe.utils.getdate():
-            frappe.throw(_("No se pueden asignar más casos a los términos de soporte seleccionados ya que están restringidos y/o han expirado."))
+        #if support_term.restringir == "Si" or support_term.fecha_de_finalizacion <= frappe.utils.getdate():
+        #    frappe.throw(_("No se pueden asignar más casos a los términos de soporte seleccionados ya que están restringidos y/o han expirado."))
         
         tah = 0
         tih = 0
