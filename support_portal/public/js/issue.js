13f38b7e8f49e38cab2ed74c8f95b40365f84009
Ashley <ashleydefort@outlook.es>
2026-01-29 09:51:16 -0500

    feat: agregar filtro de cliente support terms

--- support_portal/public/js/issue.js
+++ support_portal/public/js/issue.js
@@ -10,9 +10,16 @@ frappe.ui.form.on("Issue", {
     },
     setup: function(frm, cdt, cdn){
         frm.set_query("support_terms", function() {
-            return {
-                query:"support_portal.services.support_terms.handler"
-            };
+            if (!frm.doc.customer){
+                return{
+                    query:"support_portal.services.support_terms.handler",
+                }
+            }else{
+                return {
+                    query:"support_portal.services.support_terms.handler",
+                    filters: {"cliente": frm.doc.customer}
+                }
+            }
         });
         frm.refresh_fields()
     }
